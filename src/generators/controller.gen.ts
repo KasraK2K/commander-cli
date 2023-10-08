a8799f3211ef9b12f14634ed6494d474c487d9c3
Kasra Karami <Kasra_K2K@yahoo.com>
2023-10-08 09:58:37 +0330

    complete controller generator

---
+++ src/generators/controller.gen.ts
@@ -0,0 +1,27 @@
+/* ------------------------------ Dependencies ------------------------------ */
+import caseAnything from 'case-anything'
+/* -------------------------------------------------------------------------- */
+
+export const controllerTextGenerator = (moduleName: string): string => {
+    const pascalCase = caseAnything.pascalCase(moduleName)
+    const camelCase = caseAnything.camelCase(moduleName)
+
+    return `
+        /* ------------------------------ Dependencies ------------------------------ */
+        import { Request, Response } from 'express'
+        /* ----------------------------- Custom Modules ----------------------------- */
+        import Controller from '../../base/Controller'
+        import { BindInstance, Cache, Role } from '../../common/decorators'
+        import ${camelCase}Service from './${camelCase}.service'
+        /* -------------------------------------------------------------------------- */
+        
+        @BindInstance
+        class ${pascalCase}Controller extends Controller {
+            async healthCheck(req: Request, res: Response) {
+                await super.handle(${camelCase}Service.healthCheck, undefined, req, res)
+            }
+        }
+        
+        export default new ${pascalCase}Controller()
+    `
+}
