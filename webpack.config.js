663325e13656affbb314d7a8a1e2da0d9f6351bb
Kasra Karami <Kasra_K2K@yahoo.com>
2023-10-10 23:38:02 +0330

    change structure

--- webpack.config.js
+++ webpack.config.js
@@ -1,35 +1,23 @@
-/* ------------------------------ Node Modules ------------------------------ */
-const path = require('node:path')
 /* ------------------------------ Dependencies ------------------------------ */
 const nodeExternals = require('webpack-node-externals')
 const WebpackShellPluginNext = require('webpack-shell-plugin-next')
 const TerserPlugin = require('terser-webpack-plugin')
 /* -------------------------------------------------------------------------- */
 
+const { NODE_ENV = 'production' } = process.env
+
 module.exports = {
     entry: './src/index.ts',
-    mode: 'production',
+    mode: NODE_ENV,
     target: 'node',
     watch: false,
     externals: [nodeExternals()],
     output: {
-        path: path.resolve(process.cwd(), 'build'),
-        filename: 'src/index.js',
+        path: process.cwd(),
+        filename: 'index.js',
     },
-    resolve: {
-        alias: {
-            // '@src': path.resolve(__dirname, 'src')
-        },
-        extensions: ['.ts', '.js', '.json'],
-        modules: ['node_modules', path.resolve(__dirname, 'src')],
-    },
-    module: {
-        rules: [{ test: /\.ts$/, use: ['ts-loader'] }],
-    },
-    plugins: [
-        new WebpackShellPluginNext({
-            onAfterDone: { scripts: ['echo "Build complete!"'] },
-        }),
-    ],
+    resolve: { extensions: ['.ts', '.js', '.json'] },
+    module: { rules: [{ test: /\.ts$/, use: ['ts-loader'] }] },
+    plugins: [new WebpackShellPluginNext({ onAfterDone: { scripts: ["echo 'Build complete!'"] } })],
     optimization: { minimize: false, minimizer: [new TerserPlugin()] },
 }
