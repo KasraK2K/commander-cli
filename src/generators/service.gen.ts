05ecb2a9d1df5dba4074de3c0abfe05c6ec80b39
Kasra Karami <Kasra_K2K@yahoo.com>
2023-10-10 21:13:29 +0330

    update service generator

--- src/generators/service.gen.ts
+++ src/generators/service.gen.ts
@@ -1,5 +1,10 @@
+/* ---------------------------- Node Dependencies --------------------------- */
+import fs from 'node:fs'
 /* ------------------------------ Dependencies ------------------------------ */
 import { pascalCase, camelCase } from 'change-case-all'
+/* ----------------------------- Custom Modules ----------------------------- */
+import { getFilePath } from '../helpers'
+import { FileNames } from '../libraries'
 /* -------------------------------------------------------------------------- */
 
 export const serviceTextGenerator = (moduleName: string): string => {
@@ -7,18 +12,29 @@ export const serviceTextGenerator = (moduleName: string): string => {
     const moduleCamelCase = camelCase(moduleName)
 
     return `
-        /* ----------------------------- Custom Modules ----------------------------- */
-        import ${moduleCamelCase}Repository from './${moduleCamelCase}.repository'
-        /* -------------------------------------------------------------------------- */
-        
-        class ${modulePascalCase}Service {
-            healthCheck() {
-                return new Promise((resolve, reject) => {
-                    ${moduleCamelCase}Repository.healthCheck().then(resolve).catch(reject)
-                })
-            }
-        }
-        
-        export default new ${modulePascalCase}Service()
-    `
+/* ----------------------------- Custom Modules ----------------------------- */
+import ${moduleCamelCase}Repository from './${moduleCamelCase}.repository'
+/* -------------------------------------------------------------------------- */
+
+class ${modulePascalCase}Service {
+    healthCheck() {
+        return new Promise((resolve, reject) => {
+            ${moduleCamelCase}Repository.healthCheck().then(resolve).catch(reject)
+        })
+    }
+}
+
+export default new ${modulePascalCase}Service()
+`
+}
+
+export const createService = (moduleName: string): boolean => {
+    const serviceText = serviceTextGenerator(moduleName)
+    const filePath = getFilePath(moduleName, FileNames.SERVICE)
+    try {
+        fs.writeFileSync(filePath, serviceText)
+        return true
+    } catch {
+        return false
+    }
 }
