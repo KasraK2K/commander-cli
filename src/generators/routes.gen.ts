26cde1489ee78a333372111723e33fbea0aa0934
Kasra Karami <Kasra_K2K@yahoo.com>
2023-10-10 21:13:16 +0330

    update routes generator

--- src/generators/routes.gen.ts
+++ src/generators/routes.gen.ts
@@ -1,25 +1,41 @@
+/* ---------------------------- Node Dependencies --------------------------- */
+import fs from 'node:fs'
 /* ------------------------------ Dependencies ------------------------------ */
-import { pascalCase, camelCase } from 'change-case-all'
+import { camelCase } from 'change-case-all'
+/* ----------------------------- Custom Modules ----------------------------- */
+import { getFilePath } from '../helpers'
+import { FileNames } from '../libraries'
 /* -------------------------------------------------------------------------- */
 
 export const routesTextGenerator = (moduleName: string): string => {
     camelCase(moduleName)
 
     return `
-        /* ------------------------------ Dependencies ------------------------------ */
-        import express from 'express'
-        /* -------------------------------------------------------------------------- */
-        
-        /* -------------------------------- Constants ------------------------------- */
-        const router = express.Router()
-        /* -------------------------------------------------------------------------- */
-        
-        /* ------------------------------- Controllers ------------------------------ */
-        import ${camelCase}Controller from './${camelCase}.controller'
-        /* -------------------------------------------------------------------------- */
-        
-        router.post('/health-check', ${camelCase}Controller.healthCheck)
-        
-        export default router
-    `
+/* ------------------------------ Dependencies ------------------------------ */
+import express from 'express'
+/* -------------------------------------------------------------------------- */
+
+/* -------------------------------- Constants ------------------------------- */
+const router = express.Router()
+/* -------------------------------------------------------------------------- */
+
+/* ------------------------------- Controllers ------------------------------ */
+import ${camelCase}Controller from './${camelCase}.controller'
+/* -------------------------------------------------------------------------- */
+
+router.post('/health-check', ${camelCase}Controller.healthCheck)
+
+export default router
+`
+}
+
+export const createRoutes = (moduleName: string): boolean => {
+    const routeText = routesTextGenerator(moduleName)
+    const filePath = getFilePath(moduleName, FileNames.ROUTES)
+    try {
+        fs.writeFileSync(filePath, routeText)
+        return true
+    } catch {
+        return false
+    }
 }
