dca45b1bf11db29e09a120b8e1cbdc9bc1dbd69f
Kasra Karami <Kasra_K2K@yahoo.com>
2023-10-10 22:03:41 +0330

    update version

--- build/src/libraries/enums.js
+++ build/src/libraries/enums.js
@@ -11,7 +11,7 @@ var FileNames;
     FileNames["QUERY"] = "query";
     FileNames["MODULE"] = "module";
     FileNames["REST"] = "rest";
-})(FileNames = exports.FileNames || (exports.FileNames = {}));
+})(FileNames || (exports.FileNames = FileNames = {}));
 var LogTypes;
 (function (LogTypes) {
     LogTypes[LogTypes["VERBOSE"] = 0] = "VERBOSE";
@@ -20,4 +20,4 @@ var LogTypes;
     LogTypes[LogTypes["DEBUG"] = 3] = "DEBUG";
     LogTypes[LogTypes["ERROR"] = 4] = "ERROR";
     LogTypes[LogTypes["LOVE"] = 5] = "LOVE";
-})(LogTypes = exports.LogTypes || (exports.LogTypes = {}));
+})(LogTypes || (exports.LogTypes = LogTypes = {}));
