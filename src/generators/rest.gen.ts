dd996e48dd00641bfc7b628888349194a81ebb0a
Kasra Karami <Kasra_K2K@yahoo.com>
2023-10-10 21:13:10 +0330

    update rest generator

--- src/generators/rest.gen.ts
+++ src/generators/rest.gen.ts
@@ -1,17 +1,33 @@
+/* ---------------------------- Node Dependencies --------------------------- */
+import fs from 'node:fs'
 /* ------------------------------ Dependencies ------------------------------ */
-import { pascalCase, camelCase } from 'change-case-all'
+import { camelCase } from 'change-case-all'
+/* ----------------------------- Custom Modules ----------------------------- */
+import { getFilePath } from '../helpers'
+import { FileNames } from '../libraries'
 /* -------------------------------------------------------------------------- */
 
 export const restClientTextGenerator = (moduleName: string): string => {
     camelCase(moduleName)
 
     return `
-        @module={{host}}/${camelCase}
+@module={{host}}/${camelCase}
 
-        ###
-        
-        # @name health-check
-        GET {{module}}/health-check
-        Content-Type: application/json
-    `
+###
+
+# @name health-check
+GET {{module}}/health-check
+Content-Type: application/json
+`
+}
+
+export const createRestClient = (moduleName: string): boolean => {
+    const restText = restClientTextGenerator(moduleName)
+    const filePath = getFilePath(moduleName, FileNames.REST)
+    try {
+        fs.writeFileSync(filePath, restText)
+        return true
+    } catch {
+        return false
+    }
 }
