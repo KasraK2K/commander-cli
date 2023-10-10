1f096eef2c829520088bd8652247aff77775f542
Kasra Karami <Kasra_K2K@yahoo.com>
2023-10-10 21:12:57 +0330

    update module generator

--- src/generators/module.gen.ts
+++ src/generators/module.gen.ts
@@ -1,2 +1,25 @@
-// TODO : Create all files
-export const createModule = () => {}
+/* ----------------------------- Custom Modules ----------------------------- */
+import {
+    createRoutes,
+    createController,
+    createSchema,
+    createService,
+    createRepository,
+    createRestClient,
+} from '.'
+import { rollback } from '../helpers'
+/* -------------------------------------------------------------------------- */
+
+export const createModule = (moduleName: string): void => {
+    let error = false
+
+    // TODO : Create all files
+    if (!createController(moduleName)) error = true
+    if (!createSchema(moduleName)) error = true
+    if (!createService(moduleName)) error = true
+    if (!createRepository(moduleName)) error = true
+    if (!createRestClient(moduleName)) error = true
+    if (!createRoutes(moduleName)) error = true
+
+    if (error) rollback(moduleName)
+}
